34093d860caf39717c33d53e6d7dc52403ab1ea0
Raja <raja29cse@gmail.com>
2019-11-16 02:05:15 +0530

    Update MyClass.java
    Add method name

--- src/main/java/com/test/javalib/MyClass.java
+++ src/main/java/com/test/javalib/MyClass.java
@@ -1,4 +1,13 @@
 package com.test.javalib;
 
+/*
+@Param as any interger number less than 30
+*/
 public class MyClass {
+    public static void printSquare(int upperBound) {
+        for (int i = 0; i < upperBound; i++) {
+            System.out.println(String.format("Number:: %1$4s  Square:: %2$4s", i, i * i));
+
+        }
+    }
 }
